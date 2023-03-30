213394d77543b49d99286a47d8c99ec57e5237ec
khatch2_name <khatchshah2@gmail.com>
2023-03-30 12:00:45 +0200

    Added ViewBinding to MainActivity.kt

--- app/src/main/java/com/khatch/projektarbete_vg/MainActivity.kt
+++ app/src/main/java/com/khatch/projektarbete_vg/MainActivity.kt
@@ -2,10 +2,16 @@ package com.khatch.projektarbete_vg
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.khatch.projektarbete_vg.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    /* Initialize ViewBinding */
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        /** Set ViewBinding */
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //setContentView(R.layout.activity_main)
     }
 }
