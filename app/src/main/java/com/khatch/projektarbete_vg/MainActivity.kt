f02aed13dc9f64d7a62c728ba6ff8039925568c6
khatch2_name <khatchshah2@gmail.com>
2023-03-30 14:49:34 +0200

    Set default querySentence to "lilla fruntimmer"

--- app/src/main/java/com/khatch/projektarbete_vg/MainActivity.kt
+++ app/src/main/java/com/khatch/projektarbete_vg/MainActivity.kt
@@ -5,12 +5,18 @@ import android.os.Bundle
 import com.khatch.projektarbete_vg.counter.CounterViewModel
 import com.khatch.projektarbete_vg.databinding.ActivityMainBinding
 import androidx.activity.viewModels
+import com.khatch.projektarbete_vg.apiGoogleBooks.IGoogleBooks
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 /* https://www.googleapis.com/books/v1/volumes?q=lilla+fruntimmer */
 
 class MainActivity : AppCompatActivity() {
     /* Initialize ViewBinding */
     private lateinit var binding:ActivityMainBinding
+    private var baseUrl = "https://www.googleapis.com/"
+    private var querySentence = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         /** Set ViewBinding */
@@ -26,8 +32,24 @@ class MainActivity : AppCompatActivity() {
         val tvBookFinder = binding.tvBookFinder
         val tvGoogleBooksApi = binding.tvGoogleBooksApi // TextView
         val ivGoogleBooks = binding.ivGoogleBooks    // ImageView
+        val edEnterDesiredBook = binding.edEnterDesiredBook
+
+        if (edEnterDesiredBook.text.toString() == "") {
+            querySentence = "lilla fruntimmer"
+        }
 
         // OnClick's
         tvBookFinder.setOnClickListener() {}
+        tvGoogleBooksApi.setOnClickListener() {}
+        ivGoogleBooks.setOnClickListener() {}
+        edEnterDesiredBook.setOnClickListener() {}
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val desiredBook = retrofit.create<IGoogleBooks>().getDesiredBook()
+        
+
     }
 }
