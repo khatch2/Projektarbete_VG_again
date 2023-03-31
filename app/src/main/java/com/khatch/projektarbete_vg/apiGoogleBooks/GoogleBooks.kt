532076e5a648187e493ffb5fe72fa3fdb87ba340
khatch2_name <khatchshah2@gmail.com>
2023-03-31 16:04:31 +0200

    Added ? to variables

--- app/src/main/java/com/khatch/projektarbete_vg/apiGoogleBooks/GoogleBooks.kt
+++ app/src/main/java/com/khatch/projektarbete_vg/apiGoogleBooks/GoogleBooks.kt
@@ -2,14 +2,16 @@ package com.khatch.projektarbete_vg.apiGoogleBooks
 
 import com.google.gson.annotations.SerializedName
 
-data class GoogleBooks(var internalQuery: String = "karl") {
+data class GoogleBooks(
+    var internalQuery: String = "karl"
+    ) {
     // Template from API Object
-    @SerializedName("smallThumbnail") val myImage : String = ""
+    @SerializedName("smallThumbnail") val myImage : String? = ""
 
-    @SerializedName("previewLink") val myLink : String = ""
+    @SerializedName("previewLink") val myLink : String? = ""
 
-    @SerializedName("title") val myTitle:String=""
-    @SerializedName("authors") val myAuthors:String=""
+    @SerializedName("title") val myTitle : String? = ""
+    @SerializedName("authors") val myAuthors : String? = ""
     override fun toString(): String {
         return "GoogleBooks(myImage='$myImage', myLink='$myLink'," +
                           " myTitle='$myTitle', myAuthors='$myAuthors')"
